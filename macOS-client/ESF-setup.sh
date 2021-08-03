15bb28e146ab38f2be44abbbf22c37199a1c0cc5
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-08-03 15:07:27 -0400

    Update ESF-setup.sh

--- macOS-client/ESF-setup.sh
+++ macOS-client/ESF-setup.sh
@@ -4,8 +4,10 @@ echo "Downloading and extracting Objective See's ProcessMonitor ESF tool...\n"
 
 curl -L -k https://bitbucket.org/objective-see/deploy/downloads/ProcessMonitor_1.5.0.zip -O
 
-echo "Running ProcessMonitor..."
-unzip ProcessMonitor_1.5.0.zip && cd ProcessMonitor.app/Contents/MacOS && xattr -c ProcessMonitor && sudo ./ProcessMonitor > ~/Downloads/filebeat-7.13.4-darwin-x86_64/esf.log
+echo "ProcessMonitor has been downloaded. You will need to give it full disk access and then run it as sudo...\n"
+pwd
+
+unzip ProcessMonitor_1.5.0.zip && cd ProcessMonitor.app/Contents/MacOS && xattr -c ProcessMonitor
 
 
 
